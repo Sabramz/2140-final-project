d141a150f968f7e2dc3807b34ec4fc95d7d8067a
Scott Abramson <scottrabramson@gmail.com>
2024-03-25 20:36:38 -0400

    position rework and correct piece movement for all but king

--- game/pieces/bishop.py
+++ game/pieces/bishop.py
@@ -4,53 +4,67 @@ import os
 
 class Bishop(Piece):
 
-    def __init__(self, x, y, team):
-        super().__init__(x, y, team, "bishop")
+    def __init__(self, team):
+        super().__init__(team, "bishop")
     
     # Each piece has a different set of possible moves.
-    def possible_moves(self, board):
-        moves = []
-        x = self.x
-        y = self.y
+    def possible_moves(self, board, pos):
+        moves = set()
+        x = pos[0]
+        y = pos[1]
+        flag = False
         # up right
-        while 1 <= x and x <= 8 or 1 <= y and y <= 8:
+        while x < 8 and y < 8 and not flag:
             x += 1
             y += 1
-            moves.append((x,y))
             # If there is a piece in the way, it can be taken, but this Bishop cannot move past it
-            if not board.open_square(x,y):
+            if board.moveable((x, y), self.team):
+                moves.add((x,y))
+                if board.enemy_piece((x,y), self.team):
+                    flag = True
+            else:
                 break
-        x = self.x
-        y = self.y
+        x = pos[0]
+        y = pos[1]
+        flag = False
         # down right
-        while 1 <= x and x <= 8 or 1 <= y and y <= 8:
+        while x < 8 and 1 < y and not flag:
             x += 1
             y -= 1
-            moves.append((x,y))
-            if not board.open_square(x,y):
+            if board.moveable((x, y), self.team):
+                moves.add((x,y))
+                if board.enemy_piece((x,y), self.team):
+                    flag = True
+            else:
                 break
-        x = self.x
-        y = self.y
+        x = pos[0]
+        y = pos[1]
+        flag = False
         # down left
-        while 1 <= x and x <= 8 or 1 <= y and y <= 8:
+        while 1 < x and 1 < y  and not flag:
             x -= 1
             y -= 1
-            moves.append((x,y))
-            if not board.open_square(x,y):
+            if board.moveable((x, y), self.team):
+                moves.add((x,y))
+                if board.enemy_piece((x,y), self.team):
+                    flag = True
+            else:
                 break
-        x = self.x
-        y = self.y
+        x = pos[0]
+        y = pos[1]
+        flag = False
         # up left
-        while 1 <= x and x <= 8 or 1 <= y and y <= 8:
+        while 1 < x and y < 8 and not flag:
             x -= 1
             y += 1
-            moves.append((x,y))
-            if not board.open_square(x,y):
+            if board.moveable((x, y), self.team):
+                moves.add((x,y))
+                if board.enemy_piece((x,y), self.team):
+                    flag = True
+            else:
                 break
+            moves.add((x,y))
         return moves
-
-    def possible_moves(self, board):
-        pass
 
     def to_image(self):
         file_name = ""
--- game/pieces/knight.py
+++ game/pieces/knight.py
@@ -4,14 +4,14 @@ import os
 
 class Knight(Piece):
 
-    def __init__(self, x, y, team):
-        super().__init__(x, y, team, "knight")
+    def __init__(self, team):
+        super().__init__(team, "knight")
     
     # Each piece has a different set of possible moves.
-    def possible_moves(self, board):
-        x = self.x
-        y = self.y
-        moves = []
+    def possible_moves(self, board, pos):
+        x = pos[0]
+        y = pos[1]
+        moves = set()
         '''
         x    p   x   p   x  
         p    x   x   x   p
@@ -20,22 +20,22 @@ class Knight(Piece):
         x    p   x   p   x
         if statements go clockwise
         '''
-        if x + 1 <= 8 and y + 2 <= 8:
-            moves.append((x+1,y+2))
-        if x + 2 <= 8 and y + 1 <= 8:
-            moves.append((x+2,y+1))
-        if x + 2 <= 8 and 1 <= y - 1:
-            moves.append((x+2,y-1))
-        if x + 1 <= 8 and 1 <= y - 2:
-            moves.append((x+1,y-2))
-        if 1 <= x - 1 and 1 <= y - 2:
-            moves.append((x+1,y+2))
-        if 1 <= x - 2 and 1 <= y - 1:
-            moves.append((x+1,y+2))
-        if 1 <= x - 2 and y + 1 <= 8:
-            moves.append((x+1,y+2))
-        if 1 <= x - 1 and y + 2 <= 8:
-            moves.append((x+1,y+2))
+        if x + 1 <= 8 and y + 2 <= 8 and board.moveable((x+1, y+2), self.team):
+            moves.add((x+1,y+2))
+        if x + 2 <= 8 and y + 1 <= 8 and board.moveable((x+2, y+1), self.team):
+            moves.add((x+2,y+1))
+        if x + 2 <= 8 and 1 <= y - 1 and board.moveable((x+2, y-1), self.team):
+            moves.add((x+2,y-1))
+        if x + 1 <= 8 and 1 <= y - 2 and board.moveable((x+1, y-2), self.team):
+            moves.add((x+1,y-2))
+        if 1 <= x - 1 and 1 <= y - 2 and board.moveable((x-1, y-2), self.team):
+            moves.add((x-1,y-2))
+        if 1 <= x - 2 and 1 <= y - 1 and board.moveable((x-2, y-1), self.team):
+            moves.add((x-2,y-1))
+        if 1 <= x - 2 and y + 1 <= 8 and board.moveable((x-2, y+1), self.team):
+            moves.add((x-2,y+1))
+        if 1 <= x - 1 and y + 2 <= 8 and board.moveable((x-1, y+2), self.team):
+            moves.add((x-1,y+2))
         return moves
     
     def to_image(self):
